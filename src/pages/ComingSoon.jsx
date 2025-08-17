04b68712813cecba0fe25a6b959b84ec19066882
Joshua Liddell <joshua.liddell21@gmail.com>
2025-08-16 22:52:22 -0700

    Update ComingSoon.jsx

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -4,7 +4,7 @@ export default function ComingSoon({ onContinue }) {
   return (
     <div className="coming-soon">
       <div className="titles">
-        <h1 className="text">Website coming August 1</h1>
+        <h1 className="text">Website in Development</h1>
         <h2 className="gradient-text">Come back soon</h2>
       </div>
       <div className="foot">
