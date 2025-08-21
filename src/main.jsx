545026c4e905e5fade224e5930b6a4db79146b55
Josh-Liddell <joshua.liddell21@gmail.com>
2025-08-21 15:09:44 -0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 // scale root font-size based on monitor width
-// const screenWidth = window.screen.width;
 const screenWidth = window.screen.width;
-const scale = screenWidth < 2560 ? 0.8 : 1;
+// const screenWidth = window.screen.width;
+// const scale = screenWidth < 2560 ? 0.8 : 1;
 
-// if (screenWidth < 2560) {
-//   document.documentElement.style.fontSize = "80%";
-// } else {
-//   document.documentElement.style.fontSize = "100%";
-// }
+if (screenWidth < 2560) {
+  document.documentElement.style.fontSize = "80%";
+} else {
+  document.documentElement.style.fontSize = "100%";
+}
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -15,20 +15,20 @@ import { BrowserRouter } from "react-router-dom";
 import "./scss/main.scss";
 import App from "./App.jsx";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <div id="app-wrapper" style={{ transform: `scale(${scale})` }}>
-        <App />
-      </div>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <div id="app-wrapper" style={{ transform: `scale(${scale})` }}>
+//         <App />
+//       </div>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
